a8b7be6493ed090c9d42d1929c070533155c02f1
오준석 <y22zzp@naver.com>
2024-03-21 18:19:52 +0900

    탐욕법(그리디) 문제 재풀이 3
    회의실 배정, 풍선 맞추기, 피보나치

--- 탐욕법(Greedy) 알고리즘/문제/피보나치.js
+++ 탐욕법(Greedy) 알고리즘/문제/피보나치.js
@@ -1,27 +1,36 @@
+// 피보나치 https://www.acmicpc.net/problem/9009
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-// case 1) 권장 답안
+// 피보나치 수는 첫째 및 둘째 항이 1이며 그 뒤의 모든 항은 바로 앞 두 항의 합인 수열이다.
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, …
+
+// [문제 해결 방법]: 가능한 가장 큰 피보나치 수부터 빼 나갈 때 최소 개수를 만족.
+
+// 권장 답안
+// 피보나치 수들 계산
 pibo = [];
 pibo.push(0);
 pibo.push(1);
-while (pibo[pibo.length - 1] < 1e9)
-  pibo.push(pibo[pibo.length - 2] + pibo[pibo.length - 1]);
+
+while (pibo[pibo.length - 1] < 1e9) pibo.push(pibo[pibo.length -2] + pibo[pibo.length - 1]); // 피보나치 수 구하기
+// console.log(pibo)
+
 let testCases = Number(input[0]);
 for (let tc = 1; tc <= testCases; tc++) {
   let n = Number(input[tc]);
   let result = [];
-  let i = pibo.length - 1; // 가장 큰 피보나치 수의 인덱스
-  while (n > 0) {
-    // n이 0이 될 때까지
-    if (n >= pibo[i]) {
-      // 가능한 큰 피보나치 수부터 빼기
+  let i = pibo.length - 1; // 가장 큰 피보나치 수의 인덱스 
+
+  while (n > 0) { // n이 0이 될 때까지 
+    if (n >= pibo[i]) { // 가능한 큰 피보나치 수부터 뺀다
       n -= pibo[i];
       result.push(pibo[i]);
     }
     i--;
   }
-  let answer = "";
-  for (let i = result.length - 1; i >= 0; i--) answer += result[i] + " "; // 오름차순 출력
-  console.log(answer);
+  let answer = '';
+  for (let i = result.length - 1; i >= 0; i--) answer += result[i]+ ' '; // 오름차순 정렬
+  console.log(answer)
 }
+
--- 탐욕법(Greedy) 알고리즘/문제/회의실 배정.js
+++ 탐욕법(Greedy) 알고리즘/문제/회의실 배정.js
@@ -1,23 +1,30 @@
+// 회의실 배정 https://www.acmicpc.net/problem/1931
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-// case 1) 권장 답안
+// 첫째 줄에는 회의의 수. 이후 줄 부터(N + 1)는 회의의 시간 정보. 해당 회의가 끝나는 시간과 시작하는 시간은 중복이 가능하다. 회의의 최대 개수 계산.
+
+// 권장 답안
+// 모든 회의에 대해 오름차순 정렬. 정렬할 때는 1) 종료 시점, 2) 시작 시점을 우선순위로 함.
+// 종료시점이 이른 회의부터 확인하여 겹치지 않게 배정한다. 
+
 let n = Number(input[0]); // 회의의 개수
-let arr = []; // 각 회의에 대한 (시작 시점, 종료 시점) 입력받기
-for (let i = 1; i <= n; i++) arr.push(input[i].split(" ").map(Number));
-arr.sort(function (a, b) {
-  // (1) 종료 시점 (2) 시작 시점을 우선순위로 오름차순 정렬
+let arr = []; // 각 회의에 대한 (시작 시점, 종료 시점) 
+for (let i = 1; i <= n; i++) {
+  arr.push(input[i].split(' ').map(Number));
+}
+
+arr.sort(function(a, b) { // 종료 시간 기준으로 오름차순 정렬 
   if (a[1] != b[1]) return a[1] - b[1];
-  else return a[0] - b[0];
+  else return a[0] - b[0]
 });
 
-let cnt = 1,
-  cur = 0; // 첫 번째 회의부터 확인
+let cnt = 1, cur = 0; // 첫 번째 회의부터 확인
 for (let i = 1; i < n; i++) {
-  if (arr[cur][1] <= arr[i][0]) {
-    // 현재 회의가 끝난 이후에 회의가 시작되는 경우 카운트
+  if(arr[cur][1] <= arr[i][0]) { // 현재 회의가 끝난 이후 회의가 시작되는 경우 카운트
     cur = i;
-    cnt += 1;
+    cnt += 1
   }
 }
-console.log(cnt);
+
+console.log(cnt)
