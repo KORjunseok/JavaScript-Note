8ac807e5cd0af8075f770db2be4db801733428a7
오준석 <y22zzp@naver.com>
2024-05-01 11:17:53 +0900

    누적합 알고리즘 문제 풀이 2
    2차원 배열의 합, 부분합, 컬러볼

---
+++ 누적합 알고리즘/문제/컬러볼.js
@@ -0,0 +1,36 @@
+// 컬러볼 https://www.acmicpc.net/problem/10800
+let fs = require('fs').readFileSync('input.txt');
+let input = fs.toString().split('\n');
+
+let n = Number(input[0]); 
+let arr = [];
+for (let i = 0; i < n; i++) {
+  // 색상(c)와 크기(s)를 입력받기 
+  let c = Number(input[i + 1].split(' ')[0]);
+  let s = Number(input[i + 1].split(' ')[1]);
+  arr.push([c, s, i]);
+}
+// 크기를 기준으로 오름차순 정렬
+arr.sort((a, b) => a[1] - b[1]);
+
+let summary = 0; // 전체 누적 합 
+let colorSummary = Array(200001).fill(0); // 색상별 누적 합
+let result = Array(n).fill(0) // 공의 등장 순서(i)별 최종 결과   
+
+let start = 0;
+while (start < n) {
+  // 크기가 같은 공의 마지막 인덱스 찾기(start는 시작 인덱스 end는 끝 인덱스)
+  let end = start;
+  while (end < n && arr[start][1] == arr[end][1]) end += 1;
+  // 자기보다 작은 공들의 크기 합 - 같은 색상인 공들의 크기 합
+  for (let i = start; i < end; i++) {
+    result[arr[i][2]] = summary - colorSummary[arr[i][0]];
+  }
+  // 합계 값(누적 합) 갱신
+  for (let i = start; i < end; i++) {
+    colorSummary[arr[i][0]] += arr[i][1]; // 색상별 누적 합
+    summary += arr[i][1]; // 전체 누적합
+  }
+  start = end;
+}
+console.log(result.join('\n'));
